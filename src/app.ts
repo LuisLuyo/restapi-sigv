fe2869906c125627d0d7922c5e69fc5efc89696d
Luis Luyo <jean.luyo@bbva.com>
2021-05-19 23:26:53 -0500

    agregando puerto seguro

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,23 @@
+import fs from 'fs';
 import express, { Application } from 'express';
 import './utils/config'
 import indexRoutes from './routes/index';
 import cors from 'cors';
+import http from 'http';
+import https from 'https';
 
 const app: Application = express();
+
+// Certificate
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/frances1720.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/frances1720.com/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/frances1720.com/chain.pem', 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
 
 // middlewares
 app.use(express.json());
@@ -12,4 +26,6 @@ app.use(cors());
 // Routesnpm
 app.use(indexRoutes);
 
-export default app;
+/*export default app;*/
+export const httpServer = http.createServer(app);
+export const httpsServer = https.createServer(credentials, app);
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
-import app from "./app";
+/*import app from "./app";*/
+import { httpServer, httpsServer } from "./app";
 
 async function init() {
-    await app.listen(process.env['SERVER_PORT']);
+    await httpServer.listen(process.env['SERVER_PORT']);
     console.log('Server on port==============', process.env['SERVER_PORT']);
+    console.log('ApiRestful SIGV connect to==', process.env['AMBIENTE']);
+
+    await httpsServer.listen(process.env['SERVER_PORT_SECURE']);
+    console.log('Server on port secure ======', process.env['SERVER_PORT_SECURE']);
     console.log('ApiRestful SIGV connect to==', process.env['AMBIENTE']);
 }
 init();
